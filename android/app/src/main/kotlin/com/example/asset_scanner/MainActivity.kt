4921097645ed39aec4eceafde2ceb8507b45bd28
Kanya_N (Pa) <Kanya_N@ics-bangkok.com>
2026-03-23 12:33:54 +0700

    Add RfidSdkPlugin import to MainActivity

--- android/app/src/main/kotlin/com/example/asset_scanner/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/asset_scanner/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.asset_scanner
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import com.example.asset_scanner.RfidSdkPlugin
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
